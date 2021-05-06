e99620c30f92a577a11acf0cef466578544bde07
DREWH2023 <77160372+DREWH2023@users.noreply.github.com>
2021-05-06 13:40:24 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,65 +1,147 @@
-
-my_sprite3 = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . f f f f f f f f f f f f . .
-    . . f d d d d f d d d d d f . .
-    . . f d f f f d d d f d d f . .
-    . . f d f f f d d d f d d f . .
-    . . f d f f f d d d f d d f . .
-    . . f d f f f d d d f d d f . .
-    . . f f d d d d d d d d d f . .
-    . . f d d d d f f f d d d f . .
-    . . f d d d f 1 1 1 f d d f . .
-    . . f d d d d f f f d d d f . .
-    . . f f f f f f f f f f f f . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-"""))
+zombie: Sprite = None
 my_enemy = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . f f f f f f f f f f f f . .
-    . . f 7 7 7 7 7 7 2 2 2 2 f . .
-    . . f 7 7 f 7 7 7 f 2 2 2 f . .
-    . . f 7 7 f 7 7 7 f 7 2 2 f . .
-    . . f 7 7 f 7 7 7 f 7 7 2 f . .
-    . . f 2 2 f 7 7 7 f 7 7 7 f . .
-    . . f 2 2 7 7 7 7 7 7 7 7 f . .
-    . . f 2 7 7 f f f 7 7 7 7 f . .
-    . . f 7 7 f 7 7 7 f 7 7 2 f . .
-    . . f 7 7 7 f f f 7 7 2 2 f . .
-    . . f f f f f f f f f f f f . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-"""))
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+            . . f 7 7 f 7 7 7 f 2 2 2 f . . 
+            . . f 7 7 f 7 7 7 f 7 2 2 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 2 f . . 
+            . . f 2 2 f 7 7 7 f 7 7 7 f . . 
+            . . f 2 2 7 7 7 7 7 7 7 7 f . . 
+            . . f 2 7 7 f f f 7 7 7 7 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 2 f . . 
+            . . f 7 7 7 f f f 7 7 2 2 f . . 
+            . . f f f f f f f f f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    0)
 my_enemy2 = sprites.create(img("""
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . f f f f f f f f f f f f . .
-    . . f 2 2 7 7 7 7 7 7 7 7 f . .
-    . . f 2 7 f 7 7 7 f 7 7 7 f . .
-    . . f 7 7 f 7 7 7 f 7 7 7 f . .
-    . . f 7 7 f 7 7 7 f 7 7 7 f . .
-    . . f 7 7 f 7 7 7 f 7 7 7 f . .
-    . . f 7 7 7 7 7 7 7 7 7 7 f . .
-    . . f 2 2 2 7 7 7 7 7 7 7 f . .
-    . . f 2 2 2 2 2 2 2 2 2 2 f . .
-    . . f 2 2 2 2 2 2 2 2 2 2 f . .
-    . . f f f f f f f f f f f f . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-"""))
-
-controller.player1.move_sprite(my_sprite3, 50, 50)
-
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f 2 2 7 7 7 7 7 7 7 7 f . . 
+            . . f 2 7 f 7 7 7 f 7 7 7 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+            . . f 7 7 7 7 7 7 7 7 7 7 f . . 
+            . . f 2 2 2 7 7 7 7 7 7 7 f . . 
+            . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+            . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+            . . f f f f f f f f f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    0)
+my_enemy3 = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+            . . f 7 7 7 7 7 7 7 2 2 2 f . . 
+            . . f 7 f f f 7 7 f f 2 2 f . . 
+            . . f 7 7 7 7 7 7 7 7 7 2 f . . 
+            . . f 7 7 7 7 7 7 7 7 7 7 f . . 
+            . . f 2 7 7 f f f 7 7 7 7 f . . 
+            . . f 2 2 2 7 7 7 f 7 7 7 f . . 
+            . . f 2 2 2 2 7 7 f 7 7 7 f . . 
+            . . f 2 2 2 2 f f 7 7 7 7 f . . 
+            . . f f f f f f f f f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    0)
+maincharacter = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f d d d d f d d d d d f . . 
+            . . f d f f f d d d f d d f . . 
+            . . f d f f f d d d f d d f . . 
+            . . f d f f f d d d f d d f . . 
+            . . f d f f f d d d f d d f . . 
+            . . f f d d d d d d d d d f . . 
+            . . f d d d d f f f d d d f . . 
+            . . f d d d f 1 1 1 f d d f . . 
+            . . f d d d d f f f d d d f . . 
+            . . f f f f f f f f f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.player)
+controller.player1.move_sprite(maincharacter, 50, 50)
+info.set_score(0)
 scene.set_background_color(6)
+list2 = [img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+            . . f 7 7 f 7 7 7 f 2 2 2 f . . 
+            . . f 7 7 f 7 7 7 f 7 2 2 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 2 f . . 
+            . . f 2 2 f 7 7 7 f 7 7 7 f . . 
+            . . f 2 2 7 7 7 7 7 7 7 7 f . . 
+            . . f 2 7 7 f f f 7 7 7 7 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 2 f . . 
+            . . f 7 7 7 f f f 7 7 2 2 f . . 
+            . . f f f f f f f f f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f 2 2 7 7 7 7 7 7 7 7 f . . 
+            . . f 2 7 f 7 7 7 f 7 7 7 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+            . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+            . . f 7 7 7 7 7 7 7 7 7 7 f . . 
+            . . f 2 2 2 7 7 7 7 7 7 7 f . . 
+            . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+            . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+            . . f f f f f f f f f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . f f f f f f f f f f f f . . 
+            . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+            . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+            . . f 7 7 7 7 7 7 7 2 2 2 f . . 
+            . . f 7 f f 7 7 7 f f 2 2 f . . 
+            . . f 7 7 7 7 7 7 7 7 7 2 f . . 
+            . . f 7 7 7 f f f 7 7 7 7 f . . 
+            . . f 7 7 f 1 1 1 f 7 7 7 f . . 
+            . . f 7 7 f 1 1 2 f 7 7 7 f . . 
+            . . f 7 7 7 f f 2 2 7 7 7 f . . 
+            . . f f f f f f f f f f f f . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """)]
+speedlist = [51, 70, 30]
+turnlist = [50, 80, 40]
+maincharacter.set_bounce_on_wall(True)
+my_enemy.follow(maincharacter)
+my_enemy2.follow(maincharacter)
+my_enemy3.follow(maincharacter)
 
-my_enemy.set_position(10, 60)
-my_enemy.follow(my_sprite3, 60, 50)
-
-my_enemy2.follow(my_sprite3, 45, 75)
-my_enemy2.set_position(150, 60)
+def on_update_interval():
+    global zombie
+    zombie = sprites.create(list2._pick_random(), SpriteKind.enemy)
+    zombie.set_position(randint(0, 160), randint(0, 120))
+    info.change_score_by(1)
+game.on_update_interval(5000, on_update_interval)
--- main.ts
+++ main.ts
@@ -1,60 +1,139 @@
-let my_sprite3 = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . f f f f f f f f f f f f . .
-    . . f d d d d f d d d d d f . .
-    . . f d f f f d d d f d d f . .
-    . . f d f f f d d d f d d f . .
-    . . f d f f f d d d f d d f . .
-    . . f d f f f d d d f d d f . .
-    . . f f d d d d d d d d d f . .
-    . . f d d d d f f f d d d f . .
-    . . f d d d f 1 1 1 f d d f . .
-    . . f d d d d f f f d d d f . .
-    . . f f f f f f f f f f f f . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`)
+let zombie: Sprite = null
 let my_enemy = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . f f f f f f f f f f f f . .
-    . . f 7 7 7 7 7 7 2 2 2 2 f . .
-    . . f 7 7 f 7 7 7 f 2 2 2 f . .
-    . . f 7 7 f 7 7 7 f 7 2 2 f . .
-    . . f 7 7 f 7 7 7 f 7 7 2 f . .
-    . . f 2 2 f 7 7 7 f 7 7 7 f . .
-    . . f 2 2 7 7 7 7 7 7 7 7 f . .
-    . . f 2 7 7 f f f 7 7 7 7 f . .
-    . . f 7 7 f 7 7 7 f 7 7 2 f . .
-    . . f 7 7 7 f f f 7 7 2 2 f . .
-    . . f f f f f f f f f f f f . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`)
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f f f f . . 
+    . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+    . . f 7 7 f 7 7 7 f 2 2 2 f . . 
+    . . f 7 7 f 7 7 7 f 7 2 2 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 2 f . . 
+    . . f 2 2 f 7 7 7 f 7 7 7 f . . 
+    . . f 2 2 7 7 7 7 7 7 7 7 f . . 
+    . . f 2 7 7 f f f 7 7 7 7 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 2 f . . 
+    . . f 7 7 7 f f f 7 7 2 2 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, 0)
 let my_enemy2 = sprites.create(img`
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-    . . f f f f f f f f f f f f . .
-    . . f 2 2 7 7 7 7 7 7 7 7 f . .
-    . . f 2 7 f 7 7 7 f 7 7 7 f . .
-    . . f 7 7 f 7 7 7 f 7 7 7 f . .
-    . . f 7 7 f 7 7 7 f 7 7 7 f . .
-    . . f 7 7 f 7 7 7 f 7 7 7 f . .
-    . . f 7 7 7 7 7 7 7 7 7 7 f . .
-    . . f 2 2 2 7 7 7 7 7 7 7 f . .
-    . . f 2 2 2 2 2 2 2 2 2 2 f . .
-    . . f 2 2 2 2 2 2 2 2 2 2 f . .
-    . . f f f f f f f f f f f f . .
-    . . . . . . . . . . . . . . . .
-    . . . . . . . . . . . . . . . .
-`)
-controller.player1.moveSprite(my_sprite3, 50, 50)
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f f f f . . 
+    . . f 2 2 7 7 7 7 7 7 7 7 f . . 
+    . . f 2 7 f 7 7 7 f 7 7 7 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+    . . f 7 7 7 7 7 7 7 7 7 7 f . . 
+    . . f 2 2 2 7 7 7 7 7 7 7 f . . 
+    . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+    . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, 0)
+let my_enemy3 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f f f f . . 
+    . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+    . . f 7 7 7 7 7 7 7 2 2 2 f . . 
+    . . f 7 f f f 7 7 f f 2 2 f . . 
+    . . f 7 7 7 7 7 7 7 7 7 2 f . . 
+    . . f 7 7 7 7 7 7 7 7 7 7 f . . 
+    . . f 2 7 7 f f f 7 7 7 7 f . . 
+    . . f 2 2 2 7 7 7 f 7 7 7 f . . 
+    . . f 2 2 2 2 7 7 f 7 7 7 f . . 
+    . . f 2 2 2 2 f f 7 7 7 7 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, 0)
+let maincharacter = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f f f f . . 
+    . . f d d d d f d d d d d f . . 
+    . . f d f f f d d d f d d f . . 
+    . . f d f f f d d d f d d f . . 
+    . . f d f f f d d d f d d f . . 
+    . . f d f f f d d d f d d f . . 
+    . . f f d d d d d d d d d f . . 
+    . . f d d d d f f f d d d f . . 
+    . . f d d d f 1 1 1 f d d f . . 
+    . . f d d d d f f f d d d f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.player1.moveSprite(maincharacter, 50, 50)
+info.setScore(0)
 scene.setBackgroundColor(6)
-my_enemy.setPosition(10, 60)
-my_enemy.follow(my_sprite3, 60, 50)
-my_enemy2.follow(my_sprite3, 45, 75)
-my_enemy2.setPosition(150, 60)
+let list2 = [img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f f f f . . 
+    . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+    . . f 7 7 f 7 7 7 f 2 2 2 f . . 
+    . . f 7 7 f 7 7 7 f 7 2 2 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 2 f . . 
+    . . f 2 2 f 7 7 7 f 7 7 7 f . . 
+    . . f 2 2 7 7 7 7 7 7 7 7 f . . 
+    . . f 2 7 7 f f f 7 7 7 7 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 2 f . . 
+    . . f 7 7 7 f f f 7 7 2 2 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f f f f . . 
+    . . f 2 2 7 7 7 7 7 7 7 7 f . . 
+    . . f 2 7 f 7 7 7 f 7 7 7 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+    . . f 7 7 f 7 7 7 f 7 7 7 f . . 
+    . . f 7 7 7 7 7 7 7 7 7 7 f . . 
+    . . f 2 2 2 7 7 7 7 7 7 7 f . . 
+    . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+    . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f f f f . . 
+    . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+    . . f 7 7 7 7 7 7 2 2 2 2 f . . 
+    . . f 7 7 7 7 7 7 7 2 2 2 f . . 
+    . . f 7 f f 7 7 7 f f 2 2 f . . 
+    . . f 7 7 7 7 7 7 7 7 7 2 f . . 
+    . . f 7 7 7 f f f 7 7 7 7 f . . 
+    . . f 7 7 f 1 1 1 f 7 7 7 f . . 
+    . . f 7 7 f 1 1 2 f 7 7 7 f . . 
+    . . f 7 7 7 f f 2 2 7 7 7 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `]
+let speedlist = [51, 70, 30]
+let turnlist = [50, 80, 40]
+maincharacter.setBounceOnWall(true)
+my_enemy.follow(maincharacter)
+my_enemy2.follow(maincharacter)
+my_enemy3.follow(maincharacter)
+game.onUpdateInterval(5000, function () {
+    zombie = sprites.create(list2._pickRandom(), SpriteKind.Enemy)
+    zombie.setPosition(randint(0, 160), randint(0, 120))
+    info.changeScoreBy(1)
+})
